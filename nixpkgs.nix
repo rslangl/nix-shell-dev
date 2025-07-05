110ca02ac77666ed84b2066c5173b8477f9c0405
rslangl <151566232+rslangl@users.noreply.github.com>
2025-07-05 15:16:52 +0200

    chore: add renovate tag for nixpkgs (#5)

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,5 +1,6 @@
 let
   pkgs = import (builtins.fetchTarball {
+    # renovate: datasource=github-tags depName=nixpkgs
     url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz";
   }) {};
 in
